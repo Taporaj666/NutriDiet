a3e34971d1049aac2981ba070c5ad4a294fbb563
nicholas0486 <147629551+nicholas0486@users.noreply.github.com>
2024-01-12 10:44:04 +0800

    insert recipe

--- NutriDiet/lib/Account/Login.dart
+++ NutriDiet/lib/Account/Login.dart
@@ -99,7 +99,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         setState(() {
                           working = false;
                           error = true;
-                          message.text = "Invalid Email/Password";
+                          message.text = "Error: ${returnMessage!}";
                         });
                       }
                     }
@@ -175,7 +175,17 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
           ),
         ),
-        SizedBox(height: 15,),
+        SizedBox(height: 5,),
+        Row(
+          children: [
+            Spacer(), GestureDetector(
+              onTap: () {
+
+              },
+              child: Text("Forgot Password?", style: TextStyle(fontSize: 14, color: Color(0xff454B60)),)
+            ),
+          ],
+        )
       ],
     );
   }
